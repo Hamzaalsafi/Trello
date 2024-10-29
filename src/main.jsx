c44ca14b99edeb4869dfa8ab33cfec036e7a346f
Hamzaalsafi <hamzasf345@gmail.com>
2024-10-29 17:29:34 +0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { MenuProvider } from './MenuProvider'; 
 import App from './App.jsx';
-import ".app.css"
+
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
